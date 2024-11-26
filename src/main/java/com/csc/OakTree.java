a80641b9221868977769bc58e4b7fc3027f77e51
qf40805 <qf40805@mail.harpercollege.edu>
2024-11-26 10:39:34 -0600

    oaktree-Fayaz

--- src/main/java/com/csc/OakTree.java
+++ src/main/java/com/csc/OakTree.java
@@ -1,7 +1,102 @@
 package com.csc;
 
 public class OakTree {
-  public static void main(String[] args) {
-    System.out.println("Oak Tree!!!");
+
+public static class Squirrel {
+    private String name;
+
+    public Squirrel(String name) {
+      this.name = name;
+    }
+
+    public String getName() {
+      return name;
+    }
+
+    @Override
+    public String toString() {
+      return "Squirrel{" + "name='" + name + '\'' + '}';
+    }
+}
+
+public static class BinaryTreeNode {
+    private Squirrel squirrel;
+    private BinaryTreeNode left;
+    private BinaryTreeNode right;
+
+    public BinaryTreeNode(Squirrel squirrel) {
+      this.squirrel = squirrel;
+      this.left = null;
+      this.right = null;
+    }
+
+    public Squirrel getSquirrel() {
+      return squirrel;
+    }
+
+    public BinaryTreeNode getLeft() {
+      return left;
+    }
+
+    public void setLeft(BinaryTreeNode left) {
+      this.left = left;
+    }
+
+    public BinaryTreeNode getRight() {
+      return right;
+    }
+
+    public void setRight(BinaryTreeNode right) {
+      this.right = right;
+    }
+}
+
+public static class BinaryTree {
+  private BinaryTreeNode root;
+
+  public BinaryTree(Squirrel rootSquirrel) {
+    this.root = new BinaryTreeNode(rootSquirrel);
   }
+
+  public BinaryTreeNode getRoot() {
+    return root;
+  }
+
+  public void addNode(BinaryTreeNode parent, BinaryTreeNode newNode, boolean isLeft) {
+    if (isLeft) {
+      parent.setLeft(newNode);
+    } else {
+      parent.setRight(newNode);
+    }
+  }
+
+  public void traverseTree(BinaryTreeNode node) {
+    if (node != null) {
+      traverseTree(node.getLeft());
+      System.out.println(node.getSquirrel());
+      traverseTree(node.getRight());
+    }
+  }
+}
+
+public static void main(String[] args) {
+  Squirrel squirrel1 = new Squirrel("Squirrel 1");
+  Squirrel squirrel2 = new Squirrel("Squirrel 2");
+  Squirrel squirrel3 = new Squirrel("Squirrel 3");
+  Squirrel squirrel4 = new Squirrel("Squirrel 4");
+
+  BinaryTree tree = new BinaryTree(squirrel1);
+
+  BinaryTreeNode node2 = new BinaryTreeNode(squirrel2);
+  BinaryTreeNode node3 = new BinaryTreeNode(squirrel3);
+  BinaryTreeNode node4 = new BinaryTreeNode(squirrel4);
+
+  tree.addNode(tree.getRoot(), node2, true);  
+  tree.addNode(tree.getRoot(), node3, false); 
+  tree.addNode(node2, node4, true);        
+
+  System.out.println("Tree traversal:");
+  tree.traverseTree(tree.getRoot());
+}
+
 }
--- src/test/java/com/csc/TestOakTree.java
+++ src/test/java/com/csc/TestOakTree.java
@@ -1,21 +1,52 @@
 package com.csc;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class TestOakTree {
 
-  OakTree tree;
+    private OakTree.BinaryTree tree;
 
-  @BeforeEach
-  void setUp() {
-    tree = new OakTree();
-  }
+    @BeforeEach
+    void setUp() {
+        OakTree.Squirrel squirrel1 = new OakTree.Squirrel("Squirrel 1");
+        tree = new OakTree.BinaryTree(squirrel1);
+    }
 
-  @Test
-  void itWorks() {
-    assertEquals(true, true);
-  }
+    @Test
+    void testAddNodeToLeft() {
+        OakTree.Squirrel squirrel2 = new OakTree.Squirrel("Squirrel 2");
+        OakTree.BinaryTreeNode node2 = new OakTree.BinaryTreeNode(squirrel2);
+
+        tree.addNode(tree.getRoot(), node2, true);
+        assertEquals(squirrel2, tree.getRoot().getLeft().getSquirrel());
+    }
+
+    @Test
+    void testAddNodeToRight() {
+        OakTree.Squirrel squirrel3 = new OakTree.Squirrel("Squirrel 3");
+        OakTree.BinaryTreeNode node3 = new OakTree.BinaryTreeNode(squirrel3);
+
+        tree.addNode(tree.getRoot(), node3, false);
+        assertEquals(squirrel3, tree.getRoot().getRight().getSquirrel());
+    }
+
+    @Test
+    void testTraverseTree() {
+        OakTree.Squirrel squirrel2 = new OakTree.Squirrel("Squirrel 2");
+        OakTree.Squirrel squirrel3 = new OakTree.Squirrel("Squirrel 3");
+        OakTree.BinaryTreeNode node2 = new OakTree.BinaryTreeNode(squirrel2);
+        OakTree.BinaryTreeNode node3 = new OakTree.BinaryTreeNode(squirrel3);
+
+        tree.addNode(tree.getRoot(), node2, true);
+        tree.addNode(tree.getRoot(), node3, false);
+
+        StringBuilder sb = new StringBuilder();
+        tree.traverseTree(tree.getRoot());
+        assertTrue(sb.toString().contains("Squirrel 1"));
+        assertTrue(sb.toString().contains("Squirrel 2"));
+        assertTrue(sb.toString().contains("Squirrel 3"));
+    }
 }
